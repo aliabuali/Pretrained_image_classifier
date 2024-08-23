46016f5412e7e280a1f6e523c5bdfa8b488b7424
Ali Abu Ali <48154810+aliabuali@users.noreply.github.com>
2024-08-23 04:08:18 +0300

    Update calculates_results_stats.py

--- data/calculates_results_stats.py
+++ data/calculates_results_stats.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER:Ali Hussain Abu Ali
+# DATE CREATED: 22/08/2024                               
+# REVISED DATE: 22/08/2024
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
 #          architecture to classify the images. This function will use the 
